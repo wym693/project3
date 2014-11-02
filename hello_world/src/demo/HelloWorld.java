04c3a129e06b472ba4eca328424f7c4811377c44
OneShout <2538904474@qq.com>
2014-11-02 13:19:19 +0800

    来自沧海一声笑的提交

--- hello_world/src/demo/HelloWorld.java
+++ hello_world/src/demo/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
       System.out.println("Hello World!");
+      //code from vm
+      System.out.println("Hello World!");
 	}
 
 }
