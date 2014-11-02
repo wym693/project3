4a4e4f04c6cbf0430dde134f1c998c4cafd81197
wym693 <wenyemin693@tom.com>
2014-11-02 19:48:55 +0800

    光提交

--- hello_world/src/demo/HelloWorld.java
+++ hello_world/src/demo/HelloWorld.java
@@ -12,6 +12,8 @@ public class HelloWorld {
       System.out.println("Hello World!");
       //code from vm2
       System.out.println("Hello World!");
+      //code from guang
+      System.out.println("Hello World!");
       
 	}
 
